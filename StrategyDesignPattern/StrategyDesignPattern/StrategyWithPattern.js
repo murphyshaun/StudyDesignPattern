a65ae1f2ef2c0141a235aa068c0f44de26003204
shaun murphy <80742830+murphyshaun@users.noreply.github.com>
2023-05-10 11:19:39 +0700

    Update StrategyWithPattern.js

--- StrategyDesignPattern/StrategyDesignPattern/StrategyWithPattern.js
+++ StrategyDesignPattern/StrategyDesignPattern/StrategyWithPattern.js
@@ -1,1 +1,20 @@
+/**
+* giảm giá khi người dùng đặt trước một sản phẩm của VINFAST
+*/
+function preOrderPrice(orinialPrice) {
+  return orinialPrice*0.8;
+}
 
+/**
+* tiếp tục thêm tính năng khuyến mãi thông thường, ví dụ nếu giá gốc < 200 thì giảm 10%, còn > thì giảm tối đa 30
+*/
+function promotionPrice(orinialPrice) {
+  return orinialPrice <= 200 ? orinialPrice * 0.9 : orinialPrice - 30;
+}
+
+/**
+* giá mặc định
+*/
+function defaultPrice(originalPrice) {
+  return orinialPrice;
+}
